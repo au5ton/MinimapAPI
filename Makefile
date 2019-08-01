f6bbb8ac994bc9e73c98dfe889246cfe83871346
Austin Jackson <themacphage@gmail.com>
2019-08-01 13:21:31 -0500

    TravisCI draft

---
+++ Makefile
@@ -0,0 +1,11 @@
+ci:
+	mvn package
+	mvn install:install-file \
+	 -DgroupId=net.austinj.xaerominimap \
+	 -DartifactId=MinimapAPI \
+	 -Dversion=1.0 \
+	 -Dfile=./target/MinimapAPI-1.0-SNAPSHOT.jar \
+	 -Dpackaging=jar \
+	 -DgeneratePom=true \
+	 -DlocalRepositoryPath=./repo \
+	 -DcreateChecksum=true \
---
+++ push.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# https://gist.github.com/willprice/e07efd73fb7f13f917ea
+# https://gist.github.com/fernandezpablo85/03cf8b0cd2e7d8527063
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add maven/
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/au5ton/MinimapAPI > /dev/null 2>&1
+  # overwrite current "gh-pages" branch
+  git push -f --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_website_files
+upload_files
+
